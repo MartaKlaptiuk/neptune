4a74f8c633ecfcac89982769e1763fb95715bec0
Marta Klaptiuk <tusyvfhnf@gmail.com>
2018-05-16 17:50:29 +0300

    Made Menu, Store, Wine Club

---
+++ main.js
@@ -0,0 +1,30 @@
+		$('.box').mouseover(function(){
+		var bg = $(this).css('background-image');	  
+			$('.box').css("background", "none");
+			$('.container').css("background-image", bg);	  
+		});
+		$('.box').mouseout(function(){		  
+			$('.box').css('background', "");
+			$('.container').css('background', '');		  
+		});
+
+		$(".box").click(function(){
+			$(".divGalleryContent").toggle();
+		});
+// WINE HOVER IMG
+		$('.boxWineStore').mouseover(function(){
+		var bg = $(this).css('background-image');	  
+			$('.boxWineStore').css("background", "none");
+			$('.container').css("background-image", bg);	  
+		});
+		$('.boxWineStore').mouseout(function(){		  
+			$('.boxWineStore').css('background', "");
+			$('.container').css('background', '');		  
+		});
+
+		$(".boxWineStore").click(function(){
+			$(".boxWineContent").toggle();
+		});
+
+
+		
